00745fff8ba4a458a2c5a782b4bc77b157a126f5
Steve Krenek <steve.krenek@zyquest.com>
2012-05-17 17:25:11 -0500

    Bumping version number back down and removing getJsonSchema.

--- index.js
+++ index.js
@@ -122,9 +122,6 @@ exports.createClient = function(_options, _cb) {
           method: "DELETE",
           json: options
         }, cb);
-      },
-      getJsonSchema: function(cb) {
-        cb(null, jsonSchemas[thing.name]);
       }
     };
     return api;
